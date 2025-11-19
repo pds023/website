124882b0a313a2a9c2b966b55a177731f5d0243a
Philippe <fphilfont@gmail.com>
2025-11-19 22:17:08 +0100

    maj site

--- js/main.js
+++ js/main.js
@@ -2,32 +2,46 @@
 * Template Name: BreezyCV - Resume / CV / vCard / Portfolio Template
 * Author: LMPixels
 * Author URL: http://themeforest.net/user/lmpixels
-* Version: 1.6.0
+* Version: 1.6.0 (modifié PF 2025)
 */
 
 (function($) {
 "use strict";
-    // Portfolio subpage filters
+
+    // Portfolio subpage filters (version sans shuffle.js, basée sur data-filter et classes)
     function portfolio_init() {
-        var portfolio_grid = $('.portfolio-grid'),
-            portfolio_filter = $('.portfolio-filters');
-            
-        if (portfolio_grid) {
+        var $portfolio_grid   = $('.portfolio-grid');
+        var $portfolio_filter = $('.portfolio-filters');
 
-            portfolio_grid.shuffle({
-                speed: 450,
-                itemSelector: 'figure'
-            });
-
-            portfolio_filter.on("click", ".filter", function (e) {
-                portfolio_grid.shuffle('update');
-                e.preventDefault();
-                $('.portfolio-filters .filter').parent().removeClass('active');
-                $(this).parent().addClass('active');
-                portfolio_grid.shuffle('shuffle', $(this).attr('data-group') );
-            });
-
+        if (!$portfolio_grid.length || !$portfolio_filter.length) {
+            return;
         }
+
+        // Accessibilité : les filtres sont des boutons
+        $portfolio_filter.find('a').attr('role', 'button').attr('aria-pressed', 'false');
+        $portfolio_filter.find('li.active a').attr('aria-pressed', 'true');
+
+        $portfolio_filter.on("click", "a", function (e) {
+            e.preventDefault();
+
+            var $this  = $(this);
+            var filter = $this.data('filter');
+            var $items = $portfolio_grid.find('figure');
+
+            // Visuel : état actif
+            $this.parent().addClass('active').siblings().removeClass('active');
+
+            // Accessibilité ARIA
+            $portfolio_filter.find('a').attr('aria-pressed', 'false');
+            $this.attr('aria-pressed', 'true');
+
+            // Filtrage par classe
+            if (!filter || filter === '*') {
+                $items.show();
+            } else {
+                $items.hide().filter(filter).show();
+            }
+        });
     }
     // /Portfolio subpage filters
 
@@ -35,16 +49,20 @@
     // Hide Mobile menu
     function mobileMenuHide() {
         var windowWidth = $(window).width(),
-            siteHeader = $('#site_header');
+            $siteHeader = $('#site_header'),
+            $menuToggle = $('.menu-toggle');
 
         if (windowWidth < 1025) {
-            siteHeader.addClass('mobile-menu-hide');
-            $('.menu-toggle').removeClass('open');
+            $siteHeader.addClass('mobile-menu-hide');
+            $menuToggle.removeClass('open').attr('aria-expanded', 'false');
+
             setTimeout(function(){
-                siteHeader.addClass('animate');
+                $siteHeader.addClass('animate');
             }, 500);
         } else {
-            siteHeader.removeClass('animate');
+            // Sur desktop, on s’assure que le header est visible
+            $siteHeader.removeClass('animate mobile-menu-hide');
+            $menuToggle.removeClass('open').attr('aria-expanded', 'false');
         }
     }
     // /Hide Mobile menu
@@ -82,7 +100,10 @@
                         var messageAlert = 'alert-' + data.type;
                         var messageText = data.message;
 
-                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable">' +
+                                       '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+                                       messageText +
+                                       '</div>';
                         if (messageAlert && messageText) {
                             $('#contact_form').find('.messages').html(alertBox);
                             $('#contact_form')[0].reset();
@@ -99,19 +120,23 @@
     $(window)
         .on('load', function() { //Load
             // Animation on Page Loading
-            $(".preloader").fadeOut( 800, "linear" );
+            $(".preloader").fadeOut(800, "linear");
 
             // initializing page transition.
             var ptPage = $('.animated-sections');
             if (ptPage[0]) {
                 PageTransitions.init({
-                    menu: 'ul.main-menu',
+                    menu: 'ul.main-menu'
                 });
             }
+
+            // S'assure que le header et les scrollbars sont cohérents avec la taille
+            mobileMenuHide();
+            customScroll();
         })
         .on('resize', function() { //Resize
-             mobileMenuHide();
-             $('.animated-section').each(function() {
+            mobileMenuHide();
+            $('.animated-section').each(function() {
                 $(this).perfectScrollbar('update');
             });
             customScroll();
@@ -120,35 +145,50 @@
 
     // On Document Load
     $(document).ready(function () {
-        var movementStrength = 23;
-        var height = movementStrength / $(document).height();
-        var width = movementStrength / $(document).width();
-        $("body").on('mousemove', function(e){
-            var pageX = e.pageX - ($(document).width() / 2),
-                pageY = e.pageY - ($(document).height() / 2),
-                newvalueX = width * pageX * -1,
-                newvalueY = height * pageY * -1,
-                elements = $('.lm-animated-bg');
+        var prefersReducedMotion = window.matchMedia &&
+                                   window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
-            elements.addClass('transition');
-            elements.css({
-                "background-position": "calc( 50% + " + newvalueX + "px ) calc( 50% + " + newvalueY + "px )",
+        // Background parallax (désactivé si prefers-reduced-motion)
+        if (!prefersReducedMotion) {
+            var movementStrength = 23;
+            var height = movementStrength / $(document).height();
+            var width  = movementStrength / $(document).width();
+
+            $("body").on('mousemove', function(e){
+                var pageX = e.pageX - ($(document).width() / 2),
+                    pageY = e.pageY - ($(document).height() / 2),
+                    newvalueX = width * pageX * -1,
+                    newvalueY = height * pageY * -1,
+                    $elements = $('.lm-animated-bg');
+
+                $elements.addClass('transition');
+                $elements.css({
+                    "background-position": "calc( 50% + " + newvalueX + "px ) calc( 50% + " + newvalueY + "px )"
+                });
+
+                setTimeout(function() {
+                    $elements.removeClass('transition');
+                }, 300);
             });
-
-            setTimeout(function() {
-                elements.removeClass('transition');
-            }, 300);
-        })
+        }
 
         // Mobile menu
         $('.menu-toggle').on("click", function () {
-            $('#site_header').addClass('animate');
-            $('#site_header').toggleClass('mobile-menu-hide');
-            $('.menu-toggle').toggleClass('open');
+            var $toggle     = $(this);
+            var $siteHeader = $('#site_header');
+
+            var wasHidden   = $siteHeader.hasClass('mobile-menu-hide');
+            var willBeShown = wasHidden; // on bascule
+
+            $siteHeader.addClass('animate');
+            $siteHeader.toggleClass('mobile-menu-hide');
+            $toggle.toggleClass('open');
+
+            $toggle.attr('aria-expanded', willBeShown ? 'true' : 'false');
         });
 
         // Mobile menu hide on main menu item click
-        $('.main-menu').on("click", "a", function (e) {
+        $('.main-menu').on("click", "a", function () {
             mobileMenuHide();
         });
 
@@ -157,10 +197,10 @@
             $('#blog-sidebar').toggleClass('open');
         });
 
-        // Initialize Portfolio grid
+        // Initialize Portfolio grid (plus de shuffle, simple filtrage)
         var $portfolio_container = $(".portfolio-grid");
         $portfolio_container.imagesLoaded(function () {
-            portfolio_init(this);
+            portfolio_init();
         });
 
         // Blog grid init
@@ -171,14 +211,14 @@
 
         customScroll();
 
-        // Text rotation
+        // Text rotation (adapté à prefers-reduced-motion)
         $('.text-rotation').owlCarousel({
             loop: true,
             dots: false,
             nav: false,
             margin: 0,
             items: 1,
-            autoplay: true,
+            autoplay: !prefersReducedMotion,
             autoplayHoverPause: false,
             autoplayTimeout: 3800,
             animateOut: 'animated-section-scaleDown',
@@ -196,18 +236,18 @@
             responsive : {
                 // breakpoint from 0 up
                 0 : {
-                    items: 1,
+                    items: 1
                 },
                 // breakpoint from 480 up
                 480 : {
-                    items: 1,
+                    items: 1
                 },
                 // breakpoint from 768 up
                 768 : {
-                    items: 2,
+                    items: 2
                 },
                 1200 : {
-                    items: 2,
+                    items: 2
                 }
             }
         });
@@ -223,14 +263,14 @@
             responsive : {
                 // breakpoint from 0 up
                 0 : {
-                    items: 2,
+                    items: 2
                 },
                 // breakpoint from 768 up
                 768 : {
-                    items: 4,
+                    items: 4
                 },
                 1200 : {
-                    items: 5,
+                    items: 5
                 }
             }
         });
@@ -254,61 +294,55 @@
             type: 'image',
             removalDelay: 300,
 
-            // Class that is added to popup wrapper and background
-            // make it unique to apply your CSS animations just to this exact popup
             mainClass: 'mfp-fade',
             image: {
-                // options for image content type
                 titleSrc: 'title',
                 gallery: {
                     enabled: true
-                },
+                }
             },
 
             iframe: {
-                markup: '<div class="mfp-iframe-scaler">'+
-                        '<div class="mfp-close"></div>'+
-                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-                        '<div class="mfp-title mfp-bottom-iframe-title"></div>'+
-                      '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+                markup:
+                    '<div class="mfp-iframe-scaler">' +
+                        '<div class="mfp-close"></div>' +
+                        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+                        '<div class="mfp-title mfp-bottom-iframe-title"></div>' +
+                    '</div>',
 
                 patterns: {
                     youtube: {
-                      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-
-                      id: null, // String that splits URL in a two parts, second part should be %id%
-                      // Or null - full URL will be returned
-                      // Or a function that should return %id%, for example:
-                      // id: function(url) { return 'parsed id'; }
-
-                      src: '%id%?autoplay=1' // URL that will be set as a source for iframe.
+                        index: 'youtube.com/',
+                        id: null,
+                        src: '%id%?autoplay=1'
                     },
                     vimeo: {
-                      index: 'vimeo.com/',
-                      id: '/',
-                      src: '//player.vimeo.com/video/%id%?autoplay=1'
+                        index: 'vimeo.com/',
+                        id: '/',
+                        src: '//player.vimeo.com/video/%id%?autoplay=1'
                     },
                     gmaps: {
-                      index: '//maps.google.',
-                      src: '%id%&output=embed'
+                        index: '//maps.google.',
+                        src: '%id%&output=embed'
                     }
                 },
 
-                srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+                srcAction: 'iframe_src'
             },
 
             callbacks: {
                 markupParse: function(template, values, item) {
-                 values.title = item.el.attr('title');
+                    values.title = item.el.attr('title');
                 }
-            },
+            }
         });
 
         //Google Maps
-        if ($(".lmpixels-map")[0]){
-            var address = "Ministère de l'Économie et des Finances, Paris", //Replace with Your Address
-                address = encodeURIComponent(address),
-                src = 'https://maps.google.com/maps?q=' + address + '&amp;t=m&amp;z=16&amp;output=embed&amp;iwloc=near&output=embed';
+        if ($(".lmpixels-map")[0]) {
+            var address = "Sénat, Paris, France", // Adresse mise à jour
+                encoded = encodeURIComponent(address),
+                src = 'https://maps.google.com/maps?q=' + encoded +
+                      '&amp;t=m&amp;z=16&amp;output=embed&amp;iwloc=near&output=embed';
             $(".lmpixels-map iframe").attr("src", src);
         }
     });
